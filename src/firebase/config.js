4348663050fb7bad5d2b0d62b59b5c677f07c79c
Tisha Saha <tishasaha32@gmail.com>
2024-06-13 20:20:25 +0530

    add firebase and redux-toolkit

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBszz-NF0r_PkE4NJsFVYgt-6XLwdRs8Es",
+  authDomain: "carepath-medhack.firebaseapp.com",
+  projectId: "carepath-medhack",
+  storageBucket: "carepath-medhack.appspot.com",
+  messagingSenderId: "582022641811",
+  appId: "1:582022641811:web:5f2f64ab425ce2bdb2b566",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
